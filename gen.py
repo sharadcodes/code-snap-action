17496b4f13f0f2c0c9ea93d4538d7bdb3e032ed0
dojo <sharadcodes>
2020-04-11 17:06:12 +0530

    Updated

--- gen.py
+++ gen.py
@@ -10,17 +10,25 @@ import os
 pathlist = Path(sys.argv[1]).glob('./*.*')
 if not os.path.exists(sys.argv[2]):
     os.makedirs(sys.argv[2])
+if not os.path.exists("temp_files"):
+    os.makedirs("temp_files")
 for path in pathlist:
     path_in_str = str(path)
-    file_name_we = str(sys.argv[2]) + "/" + str(ntpath.basename(path_in_str).replace(".", "-")) + ".html"
+    file_name_we = "temp_files/" + \
+        str(ntpath.basename(path_in_str).replace(".", "-")) + ".html"
     code = open(path_in_str, "r").read()
     f = open(file_name_we, "w")
     # save html
     print("Saving %s with %s highlighting" % (file_name_we, sys.argv[3]))
     filename = ntpath.basename(path_in_str)
     lexer = get_lexer_for_filename(filename)
-    f.write((highlight(code, lexer, HtmlFormatter(full=True,highlight=sys.argv[3], linenos=True))))
+    f.write((highlight(code, lexer, HtmlFormatter(
+        full=True, highlight=sys.argv[3], linenos=True))))
     f.close()
     # gen png
-    file_name_we_for_img = str(sys.argv[2]) + "/" + str(ntpath.basename(path_in_str).replace(".", "-")) + ".png"
+    file_name_we_for_img = str(
+        sys.argv[2]) + "/" + str(ntpath.basename(path_in_str).replace(".", "-")) + ".png"
     imgkit.from_file(file_name_we, file_name_we_for_img)
+for i in Path("temp_files").glob("./*.*"):
+    os.remove(i)
+os.removedirs("temp_files")
