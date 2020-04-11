d542ea0c8b958a46eca5c75d66a8f082837e1284
SHARAD RAJ <36638057+sharadcodes@users.noreply.github.com>
2020-04-11 16:18:43 +0530

    Create gen.py

---
+++ gen.py
@@ -0,0 +1,19 @@
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+from pathlib import Path
+import sys
+import ntpath
+import imgkit
+
+pathlist = Path(sys.argv[1]).glob('./*.*')
+for path in pathlist:
+    path_in_str = str(path)
+    file_name_we = str(sys.argv[2]) + "/" + str(ntpath.basename(path_in_str).replace(".", "-")) + ".html"
+    code = open(path_in_str, "r").read()
+    f = open(file_name_we, "w")
+    print("Saving %s with %s highlighting" % (file_name_we, sys.argv[3]))
+    f.write((highlight(code, PythonLexer(), HtmlFormatter(full=True,highlight=sys.argv[3], linenos=True))))
+    f.close()
+    file_name_we_for_img = str(sys.argv[2]) + "/" + str(ntpath.basename(path_in_str).replace(".", "-")) + ".png"
+    imgkit.from_file(file_name_we, file_name_we_for_img)
